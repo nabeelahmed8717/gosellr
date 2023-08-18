755686ff742e0ed89c56ec759c183d8e33acbc68
Nabeel Ahmed <nabeel.ahmed.ceative@gmail.com>
2023-08-18 22:56:07 +0500

    implement architect

--- gosellr-fe/src/components/landingPage/landingPage.tsx
+++ gosellr-fe/src/components/landingPage/landingPage.tsx
@@ -4,6 +4,9 @@ import { Button, Col, Row } from 'antd'
 
 import bgWrapper from '../../assets/wrapper/bg-landing.svg'
 import { servicesData } from '../../mock/services'
+import hamburger from "../../assets/icons/menu-burger.svg"
+import bolt from "../../assets/icons/bolt-outlined.svg"
+import users from "../../assets/icons/users.svg"
 
 
 import arrowDown from "../../assets/icons/arrow-down-small.svg"
@@ -14,7 +17,16 @@ const LandingPage = () => {
 
   return (
     <div className='wrapper-main-landing-page'>
-      <div style={{backgroundColor:"#F7F7F7", width:"100%", height:"200px", marginTop:"20vh"}}></div>
+      <div className="category-open-bar-wrapper">
+        <div className="section-one">
+          <Button className='d-btn hamburger-btn fs-14'><img src={hamburger} width={15} height={15} alt="" />Shop By Category</Button>
+          <div className="outh-category">
+            <p><img src={bolt} width={12} height={12} alt="" />Deals Today</p>
+          </div>
+        </div>
+        <Button className='d-btn affilate-btn'><img src={users} width={12} height={12} alt="" />Affilate Program</Button>
+      </div>
+      <div style={{ backgroundColor: "#F7F7F7", width: "100%", height: "200px", marginTop: "20vh" }}></div>
     </div>
   )
 }
