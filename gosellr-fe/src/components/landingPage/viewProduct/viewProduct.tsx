054f34fec1ddae77e1b8f09783661bbf3bab0095
Nabeel Ahmed <nabeel.ahmed.ceative@gmail.com>
2023-08-30 19:44:49 +0500

    adding cart

--- gosellr-fe/src/components/landingPage/viewProduct/viewProduct.tsx
+++ gosellr-fe/src/components/landingPage/viewProduct/viewProduct.tsx
@@ -1,6 +1,6 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import "./viewProduct.scss"
-import { Button, Col, Rate, Row } from 'antd'
+import { Button, Carousel, Col, Rate, Row } from 'antd'
 import arrowDown from "../../../assets/icons/arrow-down-small.svg"
 import shippingIcon from "../../../assets/icons/shipping.svg"
 
@@ -30,48 +30,96 @@ const productImages = [
 
 const ViewProduct = () => {
 
-  const scrollContainerRef = useRef<any>(null);
-  const [scrollPosition, setScrollPosition] = useState(0);
+    const [isMobile, setIsMobile] = useState(false);
 
-  const [imageViewerUrl, setImageViewerUrl] = useState(productImages[0])
+    const scrollContainerRef = useRef<any>(null);
+    const [scrollPosition, setScrollPosition] = useState(0);
 
-  const handleScroll = (scrollOffset:any) => {
-    if (scrollContainerRef.current) {
-      const newScrollPosition = scrollPosition + scrollOffset;
-      setScrollPosition(newScrollPosition);
-      scrollContainerRef.current.scrollLeft = newScrollPosition;
-    }
-  };
+    const [imageViewerUrl, setImageViewerUrl] = useState(productImages[0])
+
+    const colorVariants = ['#2980b9', '#2ecc71', '#8e44ad'];
+    const [selectedColor, setSelectedColor] = useState('');
+
+    const handleColorSelection = (color: any) => {
+        setSelectedColor(color);
+    };
+
+    const handleScroll = (scrollOffset: any) => {
+        if (scrollContainerRef.current) {
+            const newScrollPosition = scrollPosition + scrollOffset;
+            setScrollPosition(newScrollPosition);
+            scrollContainerRef.current.scrollLeft = newScrollPosition;
+        }
+    };
+
+    const onChange = (currentSlide: number) => {
+        console.log(currentSlide);
+    };
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia('(max-width: 768px)');
+        function handleViewportChange(event: any) {
+            setIsMobile(event.matches);
+        }
+        handleViewportChange(mediaQuery);
+        mediaQuery.addListener(handleViewportChange);
+        return () => {
+            mediaQuery.removeListener(handleViewportChange);
+        };
+    }, []);
 
     return (
         <div className='view-product-main-wrapper'>
-            <Row gutter={[20, 20]}>
-                <Col md={24} lg={7}>
-                    <div className="image-viewer-flex-cards-wrapper">
-                        <div className="image-viewer-container">
-                            <img src={imageViewerUrl?.imageUrl} alt="" />
-                        </div>
-                        <div className="image-scroll-view">
-                            <Button className="scroll-button d-btn left-btn" onClick={() => handleScroll(-100)}>
-                                <img src={arrowDown} alt="" />
-                            </Button>
-                            <div className="flex-image-container" ref={scrollContainerRef}>
-                                {productImages.map((imageUrl:any, index:any) => (
-                                    <div className="box-image" key={index} onClick={()=> setImageViewerUrl(imageUrl)}>
-                                        <img src={imageUrl.imageUrl} alt="" />
+            <Row gutter={[20, 20]} style={{ margin: "0px" }}>
+                <Col sm={24} md={24} lg={7} className='wrapper-coursel-config'>
+
+                    {
+                        !isMobile ?
+                            <div className="image-viewer-flex-cards-wrapper">
+                                <div className="image-viewer-container">
+                                    <img src={imageViewerUrl?.imageUrl} alt="" />
+                                </div>
+                                <div className="image-scroll-view">
+                                    <Button className="scroll-button d-btn left-btn" onClick={() => handleScroll(-100)}>
+                                        <img src={arrowDown} alt="" />
+                                    </Button>
+                                    <div className="flex-image-container" ref={scrollContainerRef}>
+                                        {productImages.map((imageUrl: any, index: any) => (
+                                            <div className="box-image" key={index} onClick={() => setImageViewerUrl(imageUrl)}>
+                                                <img src={imageUrl.imageUrl} alt="" />
+                                            </div>
+                                        ))}
                                     </div>
-                                ))}
+                                    <Button className="scroll-button d-btn right-btn" onClick={() => handleScroll(100)}>
+                                        <img src={arrowDown} alt="" />
+                                    </Button>
+                                </div>
                             </div>
-                            <Button className="scroll-button d-btn right-btn" onClick={() => handleScroll(100)}>
-                                <img src={arrowDown} alt="" />
-                            </Button>
-                        </div>
-                    </div>
+                            :
+                            <div className="coursel-mobile">
+                                <Carousel afterChange={onChange} className='coursel-inner'>
+                                    {
+                                        productImages.map((item: any) => (
+                                            <div >
+                                                <div className='coursel-inner'>
+                                                    <img src={item.imageUrl} alt="" />
+                                                </div>
+                                            </div>
+                                        ))}
+
+                                </Carousel>
+                            </div>
+                    }
+
+
+
+
+
                 </Col>
-                <Col md={24} lg={12}>
+                <Col sm={24} md={24} lg={12}>
                     <div className="product-details-main">
                         <div className="product-main-label">
-                        P47 Black Wireless Bluetooth Headphones Over Ear Foldable Headset for Music Calling Talking FM Ratio Pubg Gaming with Mic Microphone Stereo Bass 3.5mm
+                            P47 Black Wireless Bluetooth Headphones Over Ear Foldable Headset for Music Calling Talking FM Ratio Pubg Gaming with Mic Microphone Stereo Bass 3.5mm
                         </div>
                         <div className="main-tags">
                             <div className="tsg-tag tag-free-shipping ">
@@ -81,12 +129,32 @@ const ViewProduct = () => {
                         </div>
 
                         <div className="ratings">
-                        <Rate className="product-rating" disabled defaultValue={3.5} />
-                        <p><span>367</span> Ratings</p>
+                            <Rate className="product-rating" disabled defaultValue={3.5} />
+                            <p><span>367</span> Ratings</p>
                         </div>
+
+                        <div className="price-tags">
+                            <span>$</span><p>50</p>
+                        </div>
+
+                        <div className="color-qur">
+                            <h5>Color varients (4)</h5>
+                            <div className='flex-buttons'>
+                                {colorVariants.map((color) => (
+                                    <Button
+                                        className={`${selectedColor === color ? "highlited-color" : ""}`}
+                                        key={color}
+                                        onClick={() => handleColorSelection(color)}
+                                    >
+                                        <div style={{ width: "15px", height: "15px", borderRadius: "50%", backgroundColor: color }}></div>
+                                    </Button>
+                                ))}
+                            </div>
+                        </div>
+
                     </div>
                 </Col>
-                <Col md={24} lg={5}></Col>
+                <Col sm={24} md={24} lg={5}>sdasd</Col>
             </Row>
         </div>
     )
